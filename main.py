cab2164021269d1ff0adaf116b2524ee662ebc79
Arden Rasmussen <ardenisthebest@gmail.com>
2019-10-15 10:01:03 -0700

    Added cat moddel

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ import time
 import pybullet_data
 physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
-p.setGravity(0,0,-10)
+print(pybullet_data.getDataPath())
+p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 cubeStartPos = [0,0,1]
 cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
-boxId = p.loadURDF("r2d2.urdf",cubeStartPos, cubeStartOrientation)
-for i in range (10000):
+boxId = p.loadURDF("cat.urdf",cubeStartPos, cubeStartOrientation)
+front = p.createCollisionShape(p.GEOM_CYLINDER,radius=0.25,height=1.0)
+back = p.createCollisionShape(p.GEOM_CYLINDER,radius=0.25,height=1.0)
+for i in range (10000000):
     p.stepSimulation()
     time.sleep(1./240.)
-cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
-print(cubePos,cubeOrn)
 p.disconnect()
