2f58ac7e9da685fb53f09c02a917f23aa5f68485
Arden Rasmussen <ardenisthebest@gmail.com>
2019-10-31 12:22:38 -0700

    KITTEN

--- main.py
+++ main.py
@@ -4,38 +4,156 @@ import time
 import pybullet_data
 import matplotlib.pyplot as plt
 
-d = 10
+d = 5
 g= 9.8
+
+file = open("robocat.urdf", 'w')
+
+mass = 5.0
+length= 1.0
+radius= 0.2
+theta = np.pi / 2.0;
+
+ixx = 1/4*5.0*0.2**2+1/12*5.0*1.0
+iyy = 1/4*5.0*0.2**2+1/12*5.0*1.0
+izz = 1/2*5.0*0.2**2
+
+frx, fry, frz = np.pi / 2.0, 0.0, 0.0
+ftx, fty, ftz = 0.0, length/2.0 + 0, 0.0
+brx, bry, brz = np.pi / 2.0, 0.0, 0.0
+btx, bty, btz = 0.0, -length/2.0 - 0, 0.0
+
+file.write("""
+<?xml version="1.0"?>
+<robot name="robocat">
+    <link name="frontCylinder">
+    <visual>
+        <geometry>
+            <cylinder length="{length}" radius="{radius}" />
+        </geometry>
+        <origin rpy="{frx} {fry} {frz}" xyz="{ftx} {fty} {ftz}" />
+        <material name="gen-mat">
+            <color rgba="1.0 1.0 1.0 1.0" />
+        </material>
+    </visual>
+    <collision>
+        <geometry>
+            <cylinder length="{length}" radius="{radius}" />
+        </geometry>
+        <origin rpy="{frx} {fry} {frz}" xyz="{ftx} {fty} {ftz}" />
+    </collision>
+    <inertial>
+        <mass value="{mass}" />
+        <inertia ixx="{ixx}" ixy="0.0" ixz="0.0" iyy="{iyy}" iyz="0.0" izz="{izz}" />
+        <origin rpy="{frx} {fry} {frz}" xyz="{ftx} {fty} {ftz}" />
+    </inertial>
+    </link>
+    <link name="frontCylinder-leg">
+    <visual>
+        <geometry>
+            <cylinder length="0.5" radius="0.1" />
+        </geometry>
+        <material name="gen-mat">
+            <color rgba="1.0 1.0 1.0 1.0" />
+        </material>
+        <origin rpy="0.0 0.0 0.0" xyz="0.0 {fty} {radius}" />
+    </visual>
+    </link>
+    <joint name="frontCylinder-leg-to-frontCylinder" type="fixed">
+        <parent link="frontCylinder" />
+        <child link="frontCylinder-leg" />
+        <origin xyz="0.0 {fty} {radius}" />
+    </joint>
+    <link name="backCylinder">
+    <visual>
+        <geometry>
+            <cylinder length="{length}" radius="{radius}" />
+        </geometry>
+        <origin rpy="{brx} {bry} {brz}" xyz="{btx} {bty} {btz}" />
+        <material name="gen-mat">
+            <color rgba="1.0 1.0 1.0 1.0" />
+        </material>
+    </visual>
+    <collision>
+        <geometry>
+            <cylinder length="{length}" radius="{radius}" />
+        </geometry>
+        <origin rpy="{brx} {bry} {brz}" xyz="{btx} {bty} {btz}" />
+    </collision>
+    <inertial>
+        <mass value="{mass}" />
+        <inertia ixx="{ixx}" ixy="0.0" ixz="0.0" iyy="{iyy}" iyz="0.0" izz="{izz}" />
+        <origin rpy="{brx} {bry} {brz}" xyz="{btx} {bty} {btz}" />
+    </inertial>
+    </link>
+    <link name="backCylinder-leg">
+    <visual>
+        <geometry>
+            <cylinder length="0.5" radius="0.1" />
+        </geometry>
+        <material name="gen-mat">
+            <color rgba="1.0 1.0 1.0 1.0" />
+        </material>
+        <origin rpy="0.0 0.0 0.0" xyz="0.0 {bty} {radius}" />
+    </visual>
+    </link>
+    <joint name="backCylinder-leg-to-backCylinder" type="fixed">
+        <parent link="backCylinder" />
+        <child link="backCylinder-leg" />
+        <origin xyz="0.0 {bty} {radius}" />
+    </joint>
+    <link name="centerSphere">
+    <visual>
+        <geometry>
+            <sphere radius="{radius}" />
+        </geometry>
+        <material name="gen-mat">
+            <color rgba="1.0 1.0 1.0 1.0" />
+        </material>
+        <origin rpy="0.0 0.0 0.0" xyz="0.0 0.0 0.0" />
+    </visual>
+    </link>
+    <joint name="front_to_center" type="revolute">
+        <axis xyz="1 0 0" />
+        <limit effort="0.0" lower="-1.507" upper="1.507" velocity="0.5" />
+        <parent link="centerSphere" />
+        <child link="frontCylinder" />
+        <origin xyz="0 0 0" />
+    </joint>
+    <joint name="back_to_center" type="revolute">
+        <axis xyz="0 0 1" />
+        <limit effort="0.0" lower="-1.507" upper="1.507" velocity="0.5" />
+        <parent link="centerSphere" />
+        <child link="backCylinder" />
+        <origin xyz="0 0 0" />
+    </joint>
+</robot>
+""".format(mass=mass,length=length,radius=radius,theta=theta,ixx=ixx,iyy=iyy,izz=izz,frx=frx,fry=fry,frz=frz,ftx=ftx,fty=fty,ftz=ftz,brx=brx,bry=bry,brz=brz,btx=btx,bty=bty,btz=btz))
+file.close()
 
 physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
 print(pybullet_data.getDataPath())
-# p.setGravity(0,0,-g)
-# planeId = p.loadURDF("plane.urdf")
-cubeStartPos = [0,0,0]
+p.setGravity(0,0,-g)
+planeId = p.loadURDF("plane.urdf")
+cubeStartPos = [0,0,d]
 cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
 boxId = p.loadURDF("robocat.urdf",cubeStartPos, cubeStartOrientation)
 
-theta = np.pi / 2.0;
-ix = 1/4*5.0*0.2**2+1/12*5.0*1.0
-iy = 1/4*5.0*0.2**2+1/12*5.0*1.0
-iz = 1/2*5.0*0.2**2
-ixx = (iy + iz -ix) / 2.0
-iyy = (ix + iz -iy) / 2.0
-izz = (iy + ix -iz) / 2.0
-cloud = 1 - (iyy*np.sin(theta/2.0))/(ixx+iyy+izz-(iyy*np.power(np.cos(theta/2.0),2.0)+izz*np.power(np.sin(theta/2.0),2.0)))
-t = np.sqrt(2*d/g)
+ic = iyy*np.cos(theta/2.0)**2+izz*np.sin(theta/2.0)**2
+cloud = 1 - 2 * izz / (2*ic) * np.sin(theta/2.0)
+t = np.sqrt(2*(d-1)/g) # XXX
 omega = np.pi/(t*cloud)
 
 print("THETA: {:10f}".format(theta))
-print("IX: {:10f} IY: {:10f} IZ: {:10f}".format(ix, iy, iz))
+print("IXX: {:10f} IYY: {:10f} IZZ: {:10f}".format(ixx, iyy, izz))
 print("CLOUD: {:10f}".format(cloud))
 print("T: {:10f}".format(t))
 print("OMEGA: {:10f}".format(omega))
 
 phi = 0.0
 rfps = 240
-dfps = 60
+dfps = 30
 tmax = 10
 p.setTimeStep(1.0 / rfps)
 data = []
